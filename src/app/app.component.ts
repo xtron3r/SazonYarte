5c3371408f12feb7ac7af40f5940fabd56f450e5
Exequiel <aaronleal03@gmail.com>
2024-08-19 21:11:05 -0400

    #cambios en el login y sidebar disponible solo en pagina home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor(private menu: MenuController) {}
+export class AppComponent implements OnInit {
 
-  cerrarMenu(){
-    this.menu.close();
+  private pagina = '/home';
+
+  constructor(private router:Router, private menu:MenuController) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.urlAfterRedirects === this.pagina) {
+          this.menu.enable(true);
+        } else {
+          this.menu.enable(false);
+        }
+      }
+    });
   }
 
+  cerrarMenu() {
+    this.menu.close();
+  }
 }
--- src/app/pages/miperfil/miperfil.page.ts
+++ src/app/pages/miperfil/miperfil.page.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiperfilPage implements OnInit {
 
+
+  nombreCliente:string="JEJEEJ";
+
   constructor() { }
 
   ngOnInit() {
