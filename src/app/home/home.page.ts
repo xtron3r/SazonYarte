e8f743a336ddae433714c508273532e99d6391c9
RayCL2 <bast.bascunan@duocuc.cl>
2024-08-24 18:23:18 -0400

    implementacion parcial del contexto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  nombreUsuario:string = "Victor";
+  contrasenia: string="";
+  nombre: string="";
 
-  constructor() {}
+  constructor(private router: Router, private activedrouter: ActivatedRoute) {
+    this.activedrouter.queryParams.subscribe(param =>{
+      //valido si viene o no informacion en la ruta
+      if(this.router.getCurrentNavigation()?.extras.state){
+        this.nombre = this.router.getCurrentNavigation()?.extras?.state?.['nom'];
+        this.contrasenia = this.router.getCurrentNavigation()?.extras?.state?.['com'];
+      }
+    }) 
+  }
+ 
+ 
+ 
+  ngOnInit() {
+    
+  }
 
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  contrasenia:string = "Lo espejo";
+  edad: number = 30;
+  nombre: string = "Julian";
+  arregloPersona: any = [
+    {
+      nombre: 'José',
+      apellido: 'Muñoz',
+      edad: 24
+    },
+    {
+      nombre: 'Maria',
+      apellido: 'Rodriguez',
+      edad: 25
+    },
+    {
+      nombre: 'Alma',
+      apellido: 'Cara',
+      edad: 19
+    }
+
+  ];
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+
+  irPagina(){
+    //crear mi variable de contexto
+
+    let navigationextras: NavigationExtras = {
+      state: {
+        com: this.contrasenia,
+        ed: this.edad,
+        nom: this.nombre
+      }
+
+    }
+
+
+    //crear el codigo dque quiera de la logica
+    this.router.navigate(['/home'], navigationextras);
+
   }
 
 }
