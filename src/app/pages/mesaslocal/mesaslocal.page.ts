e19887258f74cf91b8cf9a272348736a3c42e5e8
RayCL2 <bast.bascunan@duocuc.cl>
2024-08-27 21:06:42 -0400

    remover matrices del reservar mesaslocal y mesasterraza

--- src/app/pages/mesaslocal/mesaslocal.page.ts
+++ src/app/pages/mesaslocal/mesaslocal.page.ts
@@ -7,19 +7,64 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./mesaslocal.page.scss'],
 })
 export class MesaslocalPage implements OnInit {
-  //Declaramos nuestra variable de tipo matriz 
-  asientosLocal: any[][] = [
-    [{ numero: "L1", ocupado: false }, { numero: "l2", ocupado: true }, { numero: "l3", ocupado: false }, { numero: "l4", ocupado: true },{ numero: "L5", ocupado: false } ],
-    [{ numero: "L6", ocupado: true }, { numero: "L7", ocupado: false }, { numero: "L8", ocupado: false },{ numero: "L9", ocupado: false },{ numero: "L10", ocupado: false }  ],
-    [{ numero: "L11", ocupado: true }, { numero: "L12", ocupado: false }, { numero: "L13", ocupado: false }, { numero: "14", ocupado: true }, { numero: "L15", ocupado: false }  ],
-  ];
-  constructor(private menu:MenuController) { }
+
+  // Propiedades booleanas para cada botón
+  L1Selected = false;
+  L2Selected = false;
+  L3Selected = false;
+  L4Selected = false;
+  L5Selected = false;
+  L6Selected = false;
+  L7Selected = false;
+  L8Selected = false;
+  L9Selected = false;
+  L10Selected = false;
+  L11Selected = false;
+  L12Selected = false;
+  L13Selected = false;
+  L14Selected = false;
+  L15Selected = false;
+
+  constructor(private menu: MenuController) { }
 
   ngOnInit() {
     this.menu.enable(false);
   }
 
-  seleccionarAsiento(asiento: any) {
-    asiento.ocupado = !asiento.ocupado;
+  seleccionarAsiento(asiento: string) {
+    if (asiento === 'L1') this.L1Selected = !this.L1Selected;
+    if (asiento === 'L2') this.L2Selected = !this.L2Selected;
+    if (asiento === 'L3') this.L3Selected = !this.L3Selected;
+    if (asiento === 'L4') this.L4Selected = !this.L4Selected;
+    if (asiento === 'L5') this.L5Selected = !this.L5Selected;
+    if (asiento === 'L6') this.L6Selected = !this.L6Selected;
+    if (asiento === 'L7') this.L7Selected = !this.L7Selected;
+    if (asiento === 'L8') this.L8Selected = !this.L8Selected;
+    if (asiento === 'L9') this.L9Selected = !this.L9Selected;
+    if (asiento === 'L10') this.L10Selected = !this.L10Selected;
+    if (asiento === 'L11') this.L11Selected = !this.L11Selected;
+    if (asiento === 'L12') this.L12Selected = !this.L12Selected;
+    if (asiento === 'L13') this.L13Selected = !this.L13Selected;
+    if (asiento === 'L14') this.L14Selected = !this.L14Selected;
+    if (asiento === 'L15') this.L15Selected = !this.L15Selected;
+  }
+
+  isSelected(asiento: string): boolean {
+    if (asiento === 'L1') return this.L1Selected;
+    if (asiento === 'L2') return this.L2Selected;
+    if (asiento === 'L3') return this.L3Selected;
+    if (asiento === 'L4') return this.L4Selected;
+    if (asiento === 'L5') return this.L5Selected;
+    if (asiento === 'L6') return this.L6Selected;
+    if (asiento === 'L7') return this.L7Selected;
+    if (asiento === 'L8') return this.L8Selected;
+    if (asiento === 'L9') return this.L9Selected;
+    if (asiento === 'L10') return this.L10Selected;
+    if (asiento === 'L11') return this.L11Selected;
+    if (asiento === 'L12') return this.L12Selected;
+    if (asiento === 'L13') return this.L13Selected;
+    if (asiento === 'L14') return this.L14Selected;
+    if (asiento === 'L15') return this.L15Selected;
+    return false;
   }
 }
--- src/app/pages/mesasterraza/mesasterraza.page.ts
+++ src/app/pages/mesasterraza/mesasterraza.page.ts
@@ -7,19 +7,65 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./mesasterraza.page.scss'],
 })
 export class MesasterrazaPage implements OnInit {
-//Declaramos nuestra variable de tipo matriz 
-  asientosTerraza: any[][] = [
-    [{ numero: "L1", ocupado: false }, { numero: "l2", ocupado: true }, { numero: "l3", ocupado: false }, { numero: "l4", ocupado: true },{ numero: "L5", ocupado: false } ],
-    [{ numero: "L6", ocupado: true }, { numero: "L7", ocupado: false }, { numero: "L8", ocupado: false },{ numero: "L9", ocupado: false },{ numero: "L10", ocupado: false }  ],
-    [{ numero: "L11", ocupado: true }, { numero: "L12", ocupado: false }, { numero: "L13", ocupado: false }, { numero: "14", ocupado: true }, { numero: "L15", ocupado: false }  ],
-  ];
-  constructor(private menu:MenuController) { }
+
+  T1Selected = false;
+  T2Selected = false;
+  T3Selected = false;
+  T4Selected = false;
+  T5Selected = false;
+  T6Selected = false;
+  T7Selected = false;
+  T8Selected = false;
+  T9Selected = false;
+  T10Selected = false;
+  T11Selected = false;
+  T12Selected = false;
+  T13Selected = false;
+  T14Selected = false;
+  T15Selected = false;
+
+  constructor(private menu: MenuController) { }
 
   ngOnInit() {
     this.menu.enable(false);
   }
 
-  seleccionarAsiento(asiento: any) {
-    asiento.ocupado = !asiento.ocupado;
+  seleccionarAsiento(asiento: string) {
+    if (asiento === 'T1') this.T1Selected = !this.T1Selected;
+    if (asiento === 'T2') this.T2Selected = !this.T2Selected;
+    if (asiento === 'T3') this.T3Selected = !this.T3Selected;
+    if (asiento === 'T4') this.T4Selected = !this.T4Selected;
+    if (asiento === 'T5') this.T5Selected = !this.T5Selected;
+    if (asiento === 'T6') this.T6Selected = !this.T6Selected;
+    if (asiento === 'T7') this.T7Selected = !this.T7Selected;
+    if (asiento === 'T8') this.T8Selected = !this.T8Selected;
+    if (asiento === 'T9') this.T9Selected = !this.T9Selected;
+    if (asiento === 'T10') this.T10Selected = !this.T10Selected;
+    if (asiento === 'T11') this.T11Selected = !this.T11Selected;
+    if (asiento === 'T12') this.T12Selected = !this.T12Selected;
+    if (asiento === 'T13') this.T13Selected = !this.T13Selected;
+    if (asiento === 'T14') this.T14Selected = !this.T14Selected;
+    if (asiento === 'T15') this.T15Selected = !this.T15Selected;
+  }
+
+  isSelected(asiento: string): boolean {
+    if (asiento === 'T1') return this.T1Selected;
+    if (asiento === 'T2') return this.T2Selected;
+    if (asiento === 'T3') return this.T3Selected;
+    if (asiento === 'T4') return this.T4Selected;
+    if (asiento === 'T5') return this.T5Selected;
+    if (asiento === 'T6') return this.T6Selected;
+    if (asiento === 'T7') return this.T7Selected;
+    if (asiento === 'T8') return this.T8Selected;
+    if (asiento === 'T9') return this.T9Selected;
+    if (asiento === 'T10') return this.T10Selected;
+    if (asiento === 'T11') return this.T11Selected;
+    if (asiento === 'T12') return this.T12Selected;
+    if (asiento === 'T13') return this.T13Selected;
+    if (asiento === 'T14') return this.T14Selected;
+    if (asiento === 'T15') return this.T15Selected;
+    return false;
   }
 }
+
+
