0e3f6ca9cdb12ceddaf2b46a3627a382137f026c
RayCL2 <bast.bascunan@duocuc.cl>
2024-08-28 10:55:45 -0400

    cambios en las reservas de mesas

--- src/app/pages/mesasterraza/mesasterraza.page.ts
+++ src/app/pages/mesasterraza/mesasterraza.page.ts
@@ -32,21 +32,21 @@ export class MesasterrazaPage implements OnInit {
   }
 
   seleccionarAsiento(asiento: string) {
-    if (asiento === 'L1') this.T1Elegido = !this.T1Elegido;
-    if (asiento === 'L2') this.T2Elegido = !this.T2Elegido;
-    if (asiento === 'L3') this.T3Elegido = !this.T3Elegido;
-    if (asiento === 'L4') this.T4Elegido = !this.T4Elegido;
-    if (asiento === 'L5') this.T5Elegido = !this.T5Elegido;
-    if (asiento === 'L6') this.T6Elegido = !this.T6Elegido;
-    if (asiento === 'L7') this.T7Elegido = !this.T7Elegido;
-    if (asiento === 'L8') this.T8Elegido = !this.T8Elegido;
-    if (asiento === 'L9') this.T9Elegido = !this.T9Elegido;
-    if (asiento === 'L10') this.T10Elegido = !this.T10Elegido;
-    if (asiento === 'L11') this.T11Elegido = !this.T11Elegido;
-    if (asiento === 'L12') this.T12Elegido = !this.T12Elegido;
-    if (asiento === 'L13') this.T13Elegido = !this.T13Elegido;
-    if (asiento === 'L14') this.T14Elegido = !this.T14Elegido;
-    if (asiento === 'L15') this.T15Elegido = !this.T15Elegido;
+    if (asiento === 'T1') this.T1Elegido = !this.T1Elegido;
+    if (asiento === 'T2') this.T2Elegido = !this.T2Elegido;
+    if (asiento === 'T3') this.T3Elegido = !this.T3Elegido;
+    if (asiento === 'T4') this.T4Elegido = !this.T4Elegido;
+    if (asiento === 'T5') this.T5Elegido = !this.T5Elegido;
+    if (asiento === 'T6') this.T6Elegido = !this.T6Elegido;
+    if (asiento === 'T7') this.T7Elegido = !this.T7Elegido;
+    if (asiento === 'T8') this.T8Elegido = !this.T8Elegido;
+    if (asiento === 'T9') this.T9Elegido = !this.T9Elegido;
+    if (asiento === 'T10') this.T10Elegido = !this.T10Elegido;
+    if (asiento === 'T11') this.T11Elegido = !this.T11Elegido;
+    if (asiento === 'T12') this.T12Elegido = !this.T12Elegido;
+    if (asiento === 'T13') this.T13Elegido = !this.T13Elegido;
+    if (asiento === 'T14') this.T14Elegido = !this.T14Elegido;
+    if (asiento === 'T15') this.T15Elegido = !this.T15Elegido;
   }
 
   async confirmarSeleccion() {
