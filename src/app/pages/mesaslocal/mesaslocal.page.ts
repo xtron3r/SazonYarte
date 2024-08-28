31b22ed53a7b419d6a8ad2ab53853c7b1184c272
RayCL2 <bast.bascunan@duocuc.cl>
2024-08-27 22:02:54 -0400

    cambios en mesaslocal y mesasterraza

--- src/app/pages/mesaslocal/mesaslocal.page.ts
+++ src/app/pages/mesaslocal/mesaslocal.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { AlertController, MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mesaslocal',
@@ -8,63 +8,79 @@ import { MenuController } from '@ionic/angular';
 })
 export class MesaslocalPage implements OnInit {
 
-  // Propiedades booleanas para cada botón
-  L1Selected = false;
-  L2Selected = false;
-  L3Selected = false;
-  L4Selected = false;
-  L5Selected = false;
-  L6Selected = false;
-  L7Selected = false;
-  L8Selected = false;
-  L9Selected = false;
-  L10Selected = false;
-  L11Selected = false;
-  L12Selected = false;
-  L13Selected = false;
-  L14Selected = false;
-  L15Selected = false;
+  L1Elegido = false;
+  L2Elegido = false;
+  L3Elegido = false;
+  L4Elegido = false;
+  L5Elegido = false;
+  L6Elegido = false;
+  L7Elegido = false;
+  L8Elegido = false;
+  L9Elegido = false;
+  L10Elegido = false;
+  L11Elegido = false;
+  L12Elegido = false;
+  L13Elegido = false;
+  L14Elegido = false;
+  L15Elegido = false;
 
-  constructor(private menu: MenuController) { }
+  constructor(private menu: MenuController, private alertController: AlertController) { }
 
   ngOnInit() {
     this.menu.enable(false);
   }
 
   seleccionarAsiento(asiento: string) {
-    if (asiento === 'L1') this.L1Selected = !this.L1Selected;
-    if (asiento === 'L2') this.L2Selected = !this.L2Selected;
-    if (asiento === 'L3') this.L3Selected = !this.L3Selected;
-    if (asiento === 'L4') this.L4Selected = !this.L4Selected;
-    if (asiento === 'L5') this.L5Selected = !this.L5Selected;
-    if (asiento === 'L6') this.L6Selected = !this.L6Selected;
-    if (asiento === 'L7') this.L7Selected = !this.L7Selected;
-    if (asiento === 'L8') this.L8Selected = !this.L8Selected;
-    if (asiento === 'L9') this.L9Selected = !this.L9Selected;
-    if (asiento === 'L10') this.L10Selected = !this.L10Selected;
-    if (asiento === 'L11') this.L11Selected = !this.L11Selected;
-    if (asiento === 'L12') this.L12Selected = !this.L12Selected;
-    if (asiento === 'L13') this.L13Selected = !this.L13Selected;
-    if (asiento === 'L14') this.L14Selected = !this.L14Selected;
-    if (asiento === 'L15') this.L15Selected = !this.L15Selected;
+    if (asiento === 'L1') this.L1Elegido = !this.L1Elegido;
+    if (asiento === 'L2') this.L2Elegido = !this.L2Elegido;
+    if (asiento === 'L3') this.L3Elegido = !this.L3Elegido;
+    if (asiento === 'L4') this.L4Elegido = !this.L4Elegido;
+    if (asiento === 'L5') this.L5Elegido = !this.L5Elegido;
+    if (asiento === 'L6') this.L6Elegido = !this.L6Elegido;
+    if (asiento === 'L7') this.L7Elegido = !this.L7Elegido;
+    if (asiento === 'L8') this.L8Elegido = !this.L8Elegido;
+    if (asiento === 'L9') this.L9Elegido = !this.L9Elegido;
+    if (asiento === 'L10') this.L10Elegido = !this.L10Elegido;
+    if (asiento === 'L11') this.L11Elegido = !this.L11Elegido;
+    if (asiento === 'L12') this.L12Elegido = !this.L12Elegido;
+    if (asiento === 'L13') this.L13Elegido = !this.L13Elegido;
+    if (asiento === 'L14') this.L14Elegido = !this.L14Elegido;
+    if (asiento === 'L15') this.L15Elegido = !this.L15Elegido;
   }
 
-  isSelected(asiento: string): boolean {
-    if (asiento === 'L1') return this.L1Selected;
-    if (asiento === 'L2') return this.L2Selected;
-    if (asiento === 'L3') return this.L3Selected;
-    if (asiento === 'L4') return this.L4Selected;
-    if (asiento === 'L5') return this.L5Selected;
-    if (asiento === 'L6') return this.L6Selected;
-    if (asiento === 'L7') return this.L7Selected;
-    if (asiento === 'L8') return this.L8Selected;
-    if (asiento === 'L9') return this.L9Selected;
-    if (asiento === 'L10') return this.L10Selected;
-    if (asiento === 'L11') return this.L11Selected;
-    if (asiento === 'L12') return this.L12Selected;
-    if (asiento === 'L13') return this.L13Selected;
-    if (asiento === 'L14') return this.L14Selected;
-    if (asiento === 'L15') return this.L15Selected;
-    return false;
+  async confirmarSeleccion() {
+    const asientoSeleccionado = [
+      this.L1Elegido,
+      this.L2Elegido,
+      this.L3Elegido,
+      this.L4Elegido,
+      this.L5Elegido,
+      this.L5Elegido,
+      this.L7Elegido,
+      this.L8Elegido,
+      this.L9Elegido,
+      this.L10Elegido,
+      this.L11Elegido,
+      this.L12Elegido,
+      this.L13Elegido,
+      this.L14Elegido,
+      this.L15Elegido,
+    ];
+
+    if (asientoSeleccionado.every(Seleccionado => !Seleccionado)) {
+      const alert = await this.alertController.create({
+        header: 'No hay selección',
+        message: 'Por favor, selecciona al menos una mesa.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Selección Confirmada',
+        message: 'La(s) mesa(s) han sido seleccionadas.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
   }
 }
--- src/app/pages/mesasterraza/mesasterraza.page.ts
+++ src/app/pages/mesasterraza/mesasterraza.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { AlertController, MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mesasterraza',
@@ -8,63 +8,81 @@ import { MenuController } from '@ionic/angular';
 })
 export class MesasterrazaPage implements OnInit {
 
-  T1Selected = false;
-  T2Selected = false;
-  T3Selected = false;
-  T4Selected = false;
-  T5Selected = false;
-  T6Selected = false;
-  T7Selected = false;
-  T8Selected = false;
-  T9Selected = false;
-  T10Selected = false;
-  T11Selected = false;
-  T12Selected = false;
-  T13Selected = false;
-  T14Selected = false;
-  T15Selected = false;
+ 
+  T1Elegido = false;
+  T2Elegido = false;
+  T3Elegido = false;
+  T4Elegido = false;
+  T5Elegido = false;
+  T6Elegido = false;
+  T7Elegido = false;
+  T8Elegido = false;
+  T9Elegido = false;
+  T10Elegido = false;
+  T11Elegido = false;
+  T12Elegido = false;
+  T13Elegido = false;
+  T14Elegido = false;
+  T15Elegido = false;
 
-  constructor(private menu: MenuController) { }
+  constructor(private menu: MenuController, private alertController: AlertController) { }
 
   ngOnInit() {
     this.menu.enable(false);
   }
 
   seleccionarAsiento(asiento: string) {
-    if (asiento === 'T1') this.T1Selected = !this.T1Selected;
-    if (asiento === 'T2') this.T2Selected = !this.T2Selected;
-    if (asiento === 'T3') this.T3Selected = !this.T3Selected;
-    if (asiento === 'T4') this.T4Selected = !this.T4Selected;
-    if (asiento === 'T5') this.T5Selected = !this.T5Selected;
-    if (asiento === 'T6') this.T6Selected = !this.T6Selected;
-    if (asiento === 'T7') this.T7Selected = !this.T7Selected;
-    if (asiento === 'T8') this.T8Selected = !this.T8Selected;
-    if (asiento === 'T9') this.T9Selected = !this.T9Selected;
-    if (asiento === 'T10') this.T10Selected = !this.T10Selected;
-    if (asiento === 'T11') this.T11Selected = !this.T11Selected;
-    if (asiento === 'T12') this.T12Selected = !this.T12Selected;
-    if (asiento === 'T13') this.T13Selected = !this.T13Selected;
-    if (asiento === 'T14') this.T14Selected = !this.T14Selected;
-    if (asiento === 'T15') this.T15Selected = !this.T15Selected;
+    if (asiento === 'L1') this.T1Elegido = !this.T1Elegido;
+    if (asiento === 'L2') this.T2Elegido = !this.T2Elegido;
+    if (asiento === 'L3') this.T3Elegido = !this.T3Elegido;
+    if (asiento === 'L4') this.T4Elegido = !this.T4Elegido;
+    if (asiento === 'L5') this.T5Elegido = !this.T5Elegido;
+    if (asiento === 'L6') this.T6Elegido = !this.T6Elegido;
+    if (asiento === 'L7') this.T7Elegido = !this.T7Elegido;
+    if (asiento === 'L8') this.T8Elegido = !this.T8Elegido;
+    if (asiento === 'L9') this.T9Elegido = !this.T9Elegido;
+    if (asiento === 'L10') this.T10Elegido = !this.T10Elegido;
+    if (asiento === 'L11') this.T11Elegido = !this.T11Elegido;
+    if (asiento === 'L12') this.T12Elegido = !this.T12Elegido;
+    if (asiento === 'L13') this.T13Elegido = !this.T13Elegido;
+    if (asiento === 'L14') this.T14Elegido = !this.T14Elegido;
+    if (asiento === 'L15') this.T15Elegido = !this.T15Elegido;
   }
 
-  isSelected(asiento: string): boolean {
-    if (asiento === 'T1') return this.T1Selected;
-    if (asiento === 'T2') return this.T2Selected;
-    if (asiento === 'T3') return this.T3Selected;
-    if (asiento === 'T4') return this.T4Selected;
-    if (asiento === 'T5') return this.T5Selected;
-    if (asiento === 'T6') return this.T6Selected;
-    if (asiento === 'T7') return this.T7Selected;
-    if (asiento === 'T8') return this.T8Selected;
-    if (asiento === 'T9') return this.T9Selected;
-    if (asiento === 'T10') return this.T10Selected;
-    if (asiento === 'T11') return this.T11Selected;
-    if (asiento === 'T12') return this.T12Selected;
-    if (asiento === 'T13') return this.T13Selected;
-    if (asiento === 'T14') return this.T14Selected;
-    if (asiento === 'T15') return this.T15Selected;
-    return false;
+  async confirmarSeleccion() {
+    const asientoSeleccionado = [
+      this.T1Elegido,
+      this.T2Elegido,
+      this.T3Elegido,
+      this.T4Elegido,
+      this.T5Elegido,
+      this.T6Elegido,
+      this.T7Elegido,
+      this.T8Elegido,
+      this.T9Elegido,
+      this.T10Elegido,
+      this.T11Elegido,
+      this.T12Elegido,
+      this.T13Elegido,
+      this.T14Elegido,
+      this.T15Elegido,
+    ];
+
+    if (asientoSeleccionado.every(Seleccionado => !Seleccionado)) {
+      const alert = await this.alertController.create({
+        header: 'No hay selección',
+        message: 'Por favor, selecciona al menos una mesa.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Selección Confirmada',
+        message: 'La(s) mesa(s) han sido seleccionadas.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
   }
 }
 
