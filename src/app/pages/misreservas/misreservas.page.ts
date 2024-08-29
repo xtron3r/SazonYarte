0183d96158ab5b4a054a8b205b117d1d1af9509b
RayCL2 <bast.bascunan@duocuc.cl>
2024-08-28 20:45:34 -0400

    creacion mis reservas

--- src/app/pages/misreservas/misreservas.page.ts
+++ src/app/pages/misreservas/misreservas.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-misreservas',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MisreservasPage implements OnInit {
 
-  constructor() { }
+
+  reservaConfirmada = false;
+  mesaReservada: string = '';
+  diaReserva: string = '';
+  ubicacionReserva: string = '';
+
+  constructor(private router: Router, private alertController: AlertController, private menu:MenuController) {}
 
   ngOnInit() {
+    this.menu.enable(false);
+    const estadomesa = this.router.getCurrentNavigation()?.extras.state;
+    if (estadomesa) {
+      this.mesaReservada = estadomesa['mesa'];
+      this.diaReserva = estadomesa['fecha'];
+      this.ubicacionReserva = estadomesa['ubicacion'];
+      this.reservaConfirmada = true;
+    }
   }
+ 
+  cancelarReserva() {
+    this.reservaConfirmada = false;
+    this.mesaReservada = '';
+    this.diaReserva = '';
+    this.ubicacionReserva = '';
 
+    this.alertController.create({
+      header: 'Reserva Cancelada',
+      message: 'Tu reserva ha sido cancelada.',
+      buttons: ['OK'],
+    }).then(alert => alert.present());
+  }
+  
 }
