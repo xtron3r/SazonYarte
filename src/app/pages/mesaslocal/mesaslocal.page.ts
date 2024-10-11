d78e66a821cacb393fa92706ce201f4cb13697f9
Exequiel <aaronleal03@gmail.com>
2024-10-11 19:15:05 -0300

    #cambios menores

--- src/app/pages/mesaslocal/mesaslocal.page.ts
+++ src/app/pages/mesaslocal/mesaslocal.page.ts
@@ -13,35 +13,32 @@ export class MesaslocalPage implements OnInit {
 
   mesaSeleccionada: any = null;
   fechaReserva: string = ""; 
-  mesas: any = [{
-    id_mesa: '',
-    nombre: '',
-    c_sillas: '',
-    id_ubi_fk: ''
-  }];
+  mesas: any = [
+    { id_mesa: '',
+      nombre: '',
+      c_sillas: '',
+      id_ubi_fk: ''
+    }]
 
   constructor(private router: Router, private menu: MenuController, private alertController: AlertController, private bd : ServicioBDService, private storage: NativeStorage) {
-    this.bd.buscarMesa("2").then((data) => {
-      this.mesas = data;
-    })
+    
   }
 
   ngOnInit() {
     this.menu.enable(false); 
+
+    this.bd.dbState().subscribe(data => {
+      // validar si la base de datos esta lista
+      if (data) {
+        // subscribir al observable de usuarios
+        this.bd.fetchMesas().subscribe(res => {
+          this.mesas = res;
+        });
+      }
+    });
+
   }
 
-  reservarMesa(mesa: any) {
-    if (this.mesaSeleccionada == mesa) {
-        mesa.reservada = false;
-        this.mesaSeleccionada = null;
-    } else {
-        if (this.mesaSeleccionada !== null) {
-            this.mesaSeleccionada.reservada = false;
-        }
-        mesa.reservada = true;
-        this.mesaSeleccionada = mesa;
-    }
-  }
 
   async confirmarReserva() {
     if (this.mesaSeleccionada != null && this.fechaReserva != null) {
